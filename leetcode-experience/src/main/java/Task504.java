275c6eeeaf324f0daf5d81bd48f4646301f02787
SDR <50109097+dmytrosenyk@users.noreply.github.com>
2023-10-06 23:16:03 +0300

    leetcode task 504

---
+++ leetcode-experience/src/main/java/Task504.java
@@ -0,0 +1,15 @@
+/*
+Base 7
+
+Difficulty - Easy
+Runtime - 0 ms
+Memory - 40.42 MB
+Time - 2 min
+ */
+public class Task504 {
+
+  public String convertToBase7(int num) {
+
+    return Integer.toString(num, 7);
+  }
+}
