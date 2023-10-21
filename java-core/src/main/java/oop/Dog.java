1b302a50a56481da6e5ebf71ce29fba22d452210
SDR <50109097+dmytrosenyk@users.noreply.github.com>
2023-10-21 15:23:46 +0300

    java-core oop overloading

--- java-core/src/main/java/oop/Dog.java
+++ java-core/src/main/java/oop/Dog.java
@@ -12,4 +12,14 @@ public class Dog extends Animal {
 
     System.out.println("woof-woof");
   }
+
+  void jump(String meters) {
+
+    System.out.printf("Dog jumps on " + meters + " meters");
+  }
+
+  void jump(int meters) {
+
+    System.out.printf("Dog jumps on " + meters + " meters");
+  }
 }
