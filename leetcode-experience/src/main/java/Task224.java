699567f7efe45c48b92895250bd7b04479f0890d
SDR <50109097+dmytrosenyk@users.noreply.github.com>
2024-01-05 10:16:58 +0200

    leetcode task 224

---
+++ leetcode-experience/src/main/java/Task224.java
@@ -0,0 +1,48 @@
+import java.util.Stack;
+
+/*
+Basic Calculator
+
+Difficulty - Hard
+Runtime - 9 ms
+Memory - 43.78 MB
+Time - 35 min
+ */
+public class Task224 {
+
+  public int calculate(String s) {
+
+    if (s == null) {
+      return 0;
+    }
+
+    int result = 0;
+    int sign = 1;
+    int num = 0;
+
+    Stack<Integer> stack = new Stack<>();
+    stack.push(sign);
+
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      if (c >= '0' && c <= '9') {
+        num = num * 10 + (c - '0');
+      }
+      else if (c == '+' || c == '-') {
+        result += sign * num;
+        sign = stack.peek() * (c == '+' ? 1 : -1);
+        num = 0;
+      }
+      else if (c == '(') {
+        stack.push(sign);
+      }
+      else if (c == ')') {
+        stack.pop();
+      }
+    }
+
+    result += sign * num;
+    return result;
+  }
+}
